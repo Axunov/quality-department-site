350b84ea82e7fa4c3e1b552eaf4d3d372b1037ec
Axunov <ahunovi305@gmail.com>
2026-08-03 17:52:35 +0500

    Add director accreditation monitoring page

---
+++ src/app/[locale]/accreditation/director/page.tsx
@@ -0,0 +1,5 @@
+import RoleCabinet from "@/components/accreditation/RoleCabinet";
+
+export default function AccreditationDirectorPage(){
+  return <main className="min-h-screen bg-slate-50 py-12"><div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"><RoleCabinet mode="director" /></div></main>;
+}
