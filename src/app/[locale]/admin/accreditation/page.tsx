cd4d9fc336fe5c6e4470479f70c8d74825fdd02e
Axunov <ahunovi305@gmail.com>
2026-08-03 17:52:20 +0500

    Add accreditation admin review page

---
+++ src/app/[locale]/admin/accreditation/page.tsx
@@ -0,0 +1,5 @@
+import RoleCabinet from "@/components/accreditation/RoleCabinet";
+
+export default function AccreditationAdminPage(){
+  return <main className="min-h-screen bg-slate-50 py-12"><div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"><RoleCabinet mode="admin" /></div></main>;
+}
